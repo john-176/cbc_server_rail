baf783049bd90454a921c56d2e0f1dca55632974
john-176 <johnthedeveloper7@gmail.com>
2025-07-22 11:48:49 +0300

    ALLOW ORIGIN

--- cbc_project/settings.py
+++ cbc_project/settings.py
@@ -173,8 +173,7 @@ SIMPLE_JWT = {
 }
 
 #-------------------------COORS------------------------------------------------
-
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = (
     ["http://localhost:5173", "http://127.0.0.1:5173"]
