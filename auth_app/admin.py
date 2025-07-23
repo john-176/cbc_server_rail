9e498dcf11bf14eb20c38673803cb4d3871802cf
john-176 <johnthedeveloper7@gmail.com>
2025-07-23 19:57:17 +0300

    Admin panel styling

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Change admin title, header, and index title
+admin.site.site_header = "By Faith School Admins' Dashboard."
+admin.site.site_title = "ByFaith Admin Portal"
+admin.site.index_title = "Welcome Admins!"
